3dbc7204f06dcb77a03344d0c5613ecd1bd28352
susanmarie87 <susan.marie87@gmail.com>
2021-07-01 16:23:18 +0000

    Added views and routing for each html file

--- run.py
+++ run.py
@@ -10,6 +10,22 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+
+@app.route("/support")
+def about():
+    return render_template("support.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
